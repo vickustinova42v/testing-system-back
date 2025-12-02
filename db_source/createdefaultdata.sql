cabe30528c615c42c605b6374c25f8fa1f4e71a5
vya.ustinova <vya.ustinova@tensor.ru>
2025-12-03 01:27:35 +0300

    Русифицировала роли

--- db_source/createdefaultdata.sql
+++ db_source/createdefaultdata.sql
@@ -1,7 +1,7 @@
 insert into roles (name) values
-    ('admin'),
-    ('teacher'),
-    ('student');
+    ('администратор'),
+    ('преподаватель'),
+    ('студент');
 
 insert into users (first_name, last_name, email, password, role_id) values
     ('Иван', 'Иванов', 'ivan@ivan.ru', 'admin', 1);
