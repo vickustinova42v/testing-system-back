d455254c26f594dcaedbf6bdb65b7e6fd225876d
vya.ustinova <vya.ustinova@tensor.ru>
2025-12-03 00:26:08 +0300

    Создание проекта

---
+++ src/main/java/com/example/testingsystemback/TestingSystemBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.testingsystemback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingSystemBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingSystemBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testingsystemback/TestingSystemBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingsystemback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingSystemBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
