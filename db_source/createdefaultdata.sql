0a1ca2ef481fc48665ddf0254f37c2381452dfee
vya.ustinova <vya.ustinova@tensor.ru>
2025-12-03 01:24:22 +0300

    Создание дефолтных данных для таблицы пользователи

--- db_source/createdefaultdata.sql
+++ db_source/createdefaultdata.sql
@@ -2,3 +2,6 @@ insert into roles (name) values
     ('admin'),
     ('teacher'),
     ('student');
+
+insert into users (first_name, last_name, email, password, role_id) values
+    ('Иван', 'Иванов', 'ivan@ivan.ru', 'admin', 1);
