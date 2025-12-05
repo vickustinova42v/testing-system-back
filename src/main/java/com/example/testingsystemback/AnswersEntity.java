01f234dad39d04dca8d9b837198a35e3c71696d0
vya.ustinova <vya.ustinova@tensor.ru>
2025-12-05 15:57:37 +0300

    Добавила сущности

---
+++ src/main/java/com/example/testingsystemback/AnswersEntity.java
@@ -0,0 +1,4 @@
+package com.example.testingsystemback;
+
+public class AnswersEntity {
+}
---
+++ src/main/java/com/example/testingsystemback/RolesEntity.java
@@ -0,0 +1,39 @@
+package com.example.testingsystemback;
+
+import jakarta.persistence.*;
+
+@Table(name="roles")
+@Entity
+public class RolesEntity {
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    public RolesEntity() {
+    }
+
+    public RolesEntity(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
